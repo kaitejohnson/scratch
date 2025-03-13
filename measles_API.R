f509ba1659f88807847c0414210498bb23597ea8
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-03-13 09:28:01 +0000

    got example to work, might not have right url for measles

--- measles_API.R
+++ measles_API.R
@@ -12,15 +12,18 @@ library(httr)
 # Category: cases
 # API name : measles_cases_casesByOnsetWeek
 
-
-test_measles_csv <- fread("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/Measles/geography_types/Nation/geographies/England/metrics/measles_cases_casesByOnsetWeek&format=csv", quote = "")
-test_measles <- GET("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/Measles/geography_types/Nation/geographies/England/metrics/measles_cases_casesByOnsetWeek")
-test_measles$results
-
-
-
-# Use the API exmaples and see if they work 
-test_covid <- GET("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/COVID-19/geography_types/Nation/geographies/England/metrics/COVID-19_testing_PCRcountByDay")
-test_covid_csv <- fread("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/COVID-19/geography_types/Nation/geographies/England/metrics/COVID-19_testing_PCRcountByDay&format=csv", quote = "")
+# Use the API examples: this works
+test_covid <- content(GET("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/COVID-19/geography_types/Nation/geographies/England/metrics/COVID-19_testing_PCRcountByDay"))
 # Data points are supposed to be in `results`
-test_covid$results
+results_list <- test_covid$results
+df <- as.data.frame(do.call(rbind, results_list))
+
+# Try doing the same with measles, filling in the theme and sub themes from above, but swapping theme to outbreaks 
+test_measles <- content(GET("https://api.ukhsa-dashboard.data.gov.uk/themes/outbreaks/sub_themes/respiratory/topics/Measles/geography_types/Nation/geographies/England/metrics/measles_cases_casesByOnsetWeek"))
+# Go back to infectious diseases 
+test2_measles <- content(GET("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/Measles/geography_types/Nation/geographies/England/metrics/measles_cases_casesByOnsetWeek"))
+test3_measles <- content(GET("https://api.ukhsa-dashboard.data.gov.uk/themes/infectious_disease/sub_themes/respiratory/topics/Measles/geography_types/Nation/geographies/England/metrics/measles_cases_casesByOnsetWeek"))
+results_list <- test_measles$results
+df <- as.data.frame(do.call(rbind, results_list))
+
+
