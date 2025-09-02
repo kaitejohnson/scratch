49e6fe770d301f9d85f4031c990ff99722074be4
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-09-02 09:52:17 +0100

    add code

---
+++ test_censoreddistributions.jl
@@ -0,0 +1,10 @@
+using Pkg; Pkg.add("CensoredDistributions")
+# Import the package
+using CensoredDistributions
+using Distributions
+using Random
+using Plots
+
+# Set the seed for reproducibility
+Random.seed!(123)
+
