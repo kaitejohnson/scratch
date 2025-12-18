905c326aa1da5958800d089089a8aae99f45c5af
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-12-18 15:50:07 -0500

    save a figure for each location

--- check_right_trunc.R
+++ check_right_trunc.R
@@ -15,3 +15,14 @@ denver<-vintaged_datasets |> filter(location == "denver")
 
 ggplot(denver) +
   geom_line(aes(x = target_end_date, y = observation, color = factor(as_of)))
+
+for(i in 1:length(unique(vintaged_datasets$location))){
+  loc <- unique(vintaged_datasets$location)[i]
+  sel_loc<-vintaged_datasets |> filter(location == loc)
+  
+  p <- ggplot(sel_loc) +
+    geom_line(aes(x = target_end_date, y = observation, color = factor(as_of)))
+  
+  ggsave(plot = p, 
+         filename = file.path("output",glue::glue("{loc}.png")))
+}
